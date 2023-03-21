b1eeb11de4b97bb2b605cbb268195e07c37b928b
Swyftl <68863847+NandemoStudios@users.noreply.github.com>
2023-03-21 22:23:02 +0000

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,1 +1,19 @@
+import requests
 
+def CheckForUpdate(filename, url):
+    filecheck = requests.get(url)
+    file = open(filename, 'rb')
+
+    if file.read() != filecheck.text:
+        print("the file"+filename+' Needs updating')
+        file.close()
+        file = open(filename, 'w')
+        file.write(filecheck.text)
+        file.close()
+        print("the file"+filename+" Has been updated")
+    else:
+        file.close()
+
+CheckForUpdate('Installer.py', 'https://raw.githubusercontent.com/NandemoStudios/FileDownloader/main/installer.py')
+CheckForUpdate('Updater.py', 'https://raw.githubusercontent.com/FileDownloader/NandemoStudios/updater.py')
+CheckForUpdate('Setup.py', 'https://raw.githubusercontent.com/FileDownloader/NandemoStudios/setup.py')
