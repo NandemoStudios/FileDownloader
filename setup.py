ab333c78665e7a5d8550dceaeb260c8c7b8f2406
Swyftl <68863847+NandemoStudios@users.noreply.github.com>
2023-03-21 22:08:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+import requests
 
+print("Welcome to the Script Downloader")
+print("The downloader will download the assets shortly")
+
+filefound = requests.get("https://raw.githubusercontent.com/NandemoStudios/FileDownloader/main/installer.py")
+
+Installer = open("Installer.py", "w+")
+Installer.write(filefound.text)
+Installer.close()
+
+print("Download Completed")
