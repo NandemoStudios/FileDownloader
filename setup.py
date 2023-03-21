4541387165a87f3db04171d8839ade5b70e13de2
Swyftl <68863847+NandemoStudios@users.noreply.github.com>
2023-03-21 22:23:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
 import requests
+import time
+
+def Download(filename, downloadlink):
+    file = requests.get(downloadlink)
+
+    filedownloaded = open(filename, 'w+')
+    filedownloaded.write(file.text)
+    filedownloaded.close()
+
+# Gives the user a quick rundown of what is happening
 
 print("Welcome to the Script Downloader")
+time.sleep(.5)
 print("The downloader will download the assets shortly")
+time.sleep(.5)
 
-filefound = requests.get("https://raw.githubusercontent.com/NandemoStudios/FileDownloader/main/installer.py")
+# Downloads the file with the name installer.py from the url below
 
-Installer = open("Installer.py", "w+")
-Installer.write(filefound.text)
-Installer.close()
+print("Installer the installer.py file")
+time.sleep(.5)
+
+Download('Installer.py', 'https://raw.githubusercontent.com/NandemoStudios/FileDownloader/main/installer.py')
+
+print("Downlading the updater.py file")
+time.sleep(.5)
+
+Download('Updater.py', 'https://raw.githubusercontent.com/NandemoStudios/FileDownloader/main/updater.py')
+
+time.sleep(.5)
 
 print("Download Completed")
